275a5e8f3f537733b9b72f8de67e38e57869e201
Antonio <antonihipona@gmail.com>
2020-03-24 19:25:46 +0100

    Add css configuration

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const homeRouter = require("./routers/homeRouter");
-const articleRouter = require("./routers/articleRouter");
+const homeRouter = require("./src/routers/homeRouter");
+const articleRouter = require("./src/routers/articleRouter");
 require('dotenv').config()
-
 // Conectamos con la base de datos
-mongoose.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + "/" + process.env.DB_NAME, { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb://" +
+    process.env.DB_USER +
+    ":" +
+    process.env.DB_PASSWORD +
+    "@" +
+    process.env.DB_HOST +
+    "/" +
+    process.env.DB_NAME,
+  { useNewUrlParser: true }
+);
 const db = mongoose.connection;
 
 // Comprobamos la conexión
 db.once("open", () => {
-    console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB");
 });
 
 // Comprobamos si hay errores en la base de datos
-db.on("error", (err) => {
-    console.error(err);
+db.on("error", err => {
+  console.error(err);
 });
 
 // Iniciamos la aplicación
 const app = express();
 
+// Archivos estáticos
+app.use(express.static(__dirname + "/public"));
+
 // Cargamos el motor de vista
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "pug");
 
 // Routes
@@ -33,5 +45,5 @@ app.use("/article", articleRouter);
 // Iniciamos el servidor
 const port = 3000;
 app.listen(port, () => {
-    console.log("Server started on port " + port);
+  console.log("Server started on port " + port);
 });
