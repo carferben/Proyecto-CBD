c0de396609bbac2e40d1d9267b94da06e809b215
carferben <carferben11@gmail.com>
2020-03-27 16:32:52 +0100

    Add shop show

--- src/routers/tiendaRouter.js
+++ src/routers/tiendaRouter.js
@@ -3,6 +3,18 @@ var router = express.Router();
 const mongoose = require("mongoose");
 const Tienda = require("../models/tienda");
 
+router.get("/mostrar/:id", (req, res) => {
+  console.log(req.params.id)
+  Tienda.findById(req.params.id, function(err, tienda){
+    if(err) {
+      throw err;
+    } else {
+      return res.render('tienda/mostrar', {tienda: tienda});
+    }
+  });
+});
+
+
 router.get("/add", (req, res) => {
   // var tienda1 = new Tienda({
   //   _id: new mongoose.Types.ObjectId(),
