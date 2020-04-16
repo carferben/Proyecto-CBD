8b171097381cac3ce3525029ed6dc0aff712bbc9
Antonio <antonihipona@gmail.com>
2020-04-16 13:46:54 +0200

    Add user and email unique registration errors

--- src/passport/registro.js
+++ src/passport/registro.js
@@ -1,62 +1,50 @@
 var LocalStrategy = require("passport-local").Strategy;
 var Usuario = require("../models/usuario");
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
 var bCrypt = require("bcrypt");
 
-module.exports = function(passport) {
+module.exports = function (passport) {
   passport.use(
     "registro",
     new LocalStrategy(
       {
         usernameField: "nombre_de_usuario",
         passwordField: "password",
-        passReqToCallback: true // allows us to pass back the entire request to the callback
+        passReqToCallback: true, // allows us to pass back the entire request to the callback
       },
-      function(req, nombre_de_usuario, password, done) {
-        Usuario.findOne({ nombre_de_usuario: nombre_de_usuario }, function(
-          err,
-          usuario
-        ) {
+      function (req, nombre_de_usuario, password, done) {
+        var newUser = new Usuario();
+
+        newUser._id = new mongoose.Types.ObjectId();
+        newUser.nombre_de_usuario = req.body.nombre_de_usuario;
+        newUser.password = createHash(password);
+        newUser.email = req.body.email;
+        newUser.nombre = req.body.nombre;
+        newUser.apellidos = req.body.apellidos;
+        newUser.dni = req.body.dni;
+        newUser.rol = "TIENDA";
+
+        newUser.save(function (err) {
           if (err) {
-            console.log("Error de registro: " + err);
-            return done(err);
-          }
-          if (usuario) {
-            console.log(
-              "Ya existe el usuario con el nombre: " + nombre_de_usuario
-            );
+            var errorMessage = err.errmsg;
+            if (errorMessage.includes("nombre_de_usuario"))
+              errorMessage = "user_exists";
+            if (errorMessage.includes("email"))
+              errorMessage = "email_exists";
             return done(
               null,
               false,
-              req.flash("message", "El usuario ya existe")
+              req.flash("message", errorMessage)
             );
-          } else {
-            var newUser = new Usuario();
-
-            newUser._id = new mongoose.Types.ObjectId();
-            newUser.nombre_de_usuario = req.body.nombre_de_usuario;
-            newUser.password = createHash(password);
-            newUser.email = req.body.email;
-            newUser.nombre = req.body.nombre;
-            newUser.apellidos = req.body.apellidos;
-            newUser.dni = req.body.dni;
-            newUser.rol = "TIENDA";
-
-            newUser.save(function(err) {
-              if (err) {
-                console.log("Error al registrar usuario: " + err);
-                throw err;
-              }
-              console.log("Registro de usuario exitoso");
-              return done(null, newUser);
-            });
           }
+          console.log("Registro de usuario exitoso");
+          return done(null, newUser);
         });
       }
     )
   );
 
-  var createHash = function(password) {
+  var createHash = function (password) {
     return bCrypt.hashSync(password, 10, null);
   };
 };
